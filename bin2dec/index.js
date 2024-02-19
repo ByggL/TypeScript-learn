95a706cadcb849f17c065eda4e98475582836bc3
MrSc33nsh0t <76527824+MrSc33nsh0t@users.noreply.github.com>
2024-02-19 22:25:39 +0100

    convertor finished

--- bin2dec/index.js
+++ bin2dec/index.js
@@ -1,17 +1,27 @@
 "use strict";
-var _a;
-(_a = document.getElementById("mainForm")) === null || _a === void 0 ? void 0 : _a.addEventListener("submit", (e) => {
+function submitted(e) {
+    console.log("submit !");
     e.preventDefault();
     let binaryField = document.getElementById("binary");
     let decimalField = document.getElementById("decimal");
-    if (binaryField.innerText.match(/^[0-1]+$/g) === null) {
-        Error('Enter either 0 or 1');
+    if (binaryField.value.match(/^[0-1]+$/g) === null) {
+        decimalField.innerText = "Error : input is not binary";
         return;
     }
-    const reversedBinaryText = binaryField.innerText
-        .split('')
-        .map(Number) // Convert to a number from string
-        .reverse();
-    const result = reversedBinaryText.reduce((accumulator, currentValue, idx) => accumulator + currentValue * Math.pow(2, idx));
+    let reversedBinaryText = binaryField.value.split('').map(Number).reverse();
+    console.log(reversedBinaryText);
+    let result = 0;
+    for (let i = 0; i < reversedBinaryText.length; i++) {
+        result += reversedBinaryText[i] * Math.pow(2, i);
+    }
+    // const result = reversedBinaryText.reduce(
+    //   (accumulator, currentValue, idx) =>
+    //     accumulator + parseInt(currentValue) * Math.pow(2, idx)
+    // );
+    console.log(result);
     decimalField.innerText = result.toString();
-});
+}
+window.onload = function () {
+    var form = document.getElementById("mainForm");
+    form.onsubmit = submitted.bind(form);
+};
--- bin2dec/index.ts
+++ bin2dec/index.ts
@@ -1,26 +1,26 @@
+function submitted(e: any) {
+  e.preventDefault();
+
+  let binaryField = document.getElementById("binary") as HTMLInputElement;
+  let decimalField = document.getElementById("decimal") as HTMLElement;
+
+  if (binaryField.value.match(/^[0-1]+$/g) === null) {
+      decimalField.innerText = "Error : input is not binary";
+      return;
+  }
+
+  let reversedBinaryText = binaryField.value.split('').map(Number).reverse(); // converts the input into an array of 1 and 0
+
+  let result: number = 0;
+  for(let i:number = 0; i<reversedBinaryText.length; i++) {  // converts the arrayed binary number into a decimal number
+    result += reversedBinaryText[i] * Math.pow(2,i);
+  }
+
+  decimalField.innerText = result.toString();
+}
 
 
-
-document.getElementById("mainForm")?.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    let binaryField = document.getElementById("binary") as HTMLElement;
-    let decimalField = document.getElementById("decimal") as HTMLElement;
-
-    if (binaryField.innerText.match(/^[0-1]+$/g) === null) {
-        Error('Enter either 0 or 1');
-        return;
-    }
-
-    const reversedBinaryText = binaryField.innerText
-      .split('')
-      .map(Number) // Convert to a number from string
-      .reverse();
-
-    const result = reversedBinaryText.reduce(
-      (accumulator, currentValue, idx) =>
-        accumulator + currentValue * Math.pow(2, idx)
-    );
-
-    decimalField.innerHTML = result.toString();
-});
+window.onload = function() {
+  var form = document.getElementById("mainForm")  as HTMLInputElement;
+  form.onsubmit = submitted.bind(form);
+}
